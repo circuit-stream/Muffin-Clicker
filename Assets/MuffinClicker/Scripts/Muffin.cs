25963535c30c0cd1c8c074bdc5af06e928d3fb0f
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2020-10-19 17:52:03 -0300

    Milestone 1 - Click logic

---
+++ Assets/MuffinClicker/Scripts/Muffin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Muffin : MonoBehaviour
+{
+    public int MuffinPerClick = 1;
+    public TMP_Text MuffinAmountText;
+
+    private double muffinAmount;
+
+    public void OnClick()
+    {
+        AddMuffins(MuffinPerClick);
+    }
+
+    public void Awake()
+    {
+        SetMuffins(0);
+    }
+
+    private void AddMuffins(double addedValue)
+    {
+        SetMuffins(muffinAmount + addedValue);
+    }
+
+    private void SetMuffins(double newValue)
+    {
+        muffinAmount = newValue;
+        MuffinAmountText.text = muffinAmount + " muffins";
+    }
+}
